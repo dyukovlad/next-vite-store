a4dedf0d2599d5f3310da5189e4ce410703b354d
Vladislav Dyukov <dyukovlad@gmail.com>
2025-11-19 10:24:04 +0300

    del double types

--- src/types/product.ts
+++ src/types/product.ts
@@ -40,48 +40,8 @@ interface Product {
   thumbnail: string;
 }
 
-interface ProductState {
-  products: Product[];
-  allProducts: Product[];
-  loading: boolean;
-  error: string | null;
-  hasMore: boolean;
-  skip: number;
-  searchQuery: string;
-  selectedProduct: Product | null;
-}
-
-type ProductAction =
-  | { type: 'FETCH_START' }
-  | { type: 'FETCH_SUCCESS'; payload: Product[] }
-  | { type: 'FETCH_ERROR'; payload: string }
-  | { type: 'SET_SEARCH'; payload: string }
-  | {
-      type: 'SET_FILTERED_PRODUCTS';
-      payload: { products: Product[]; hasMore: boolean; skip: number };
-    }
-  | { type: 'SET_SELECTED_PRODUCT'; payload: Product | null };
-
-interface ProductContextType {
-  products: Product[];
-  loading: boolean;
-  error: string | null;
-  hasMore: boolean;
-  fetchMoreProducts: () => void;
-  searchQuery: string;
-  setSearchQuery: (query: string) => void;
-  selectedProduct: Product | null;
-  setSelectedProduct: (product: Product | null) => void;
-}
-
 interface ProductProviderProps {
   children: ReactNode;
 }
 
-export type {
-  Product,
-  ProductState,
-  ProductAction,
-  ProductContextType,
-  ProductProviderProps,
-};
+export type { Product, ProductProviderProps };
